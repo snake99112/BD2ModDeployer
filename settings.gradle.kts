37ed872c1111800d71279613b9bf7db8ecd49651
snake99112 <67004532@qq.com>
2026-08-23 18:32:15 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { url = uri("https://jitpack.io") }
     }
 }
 
@@ -12,7 +11,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        maven("https://jitpack.io") // <-- 关键：加上这一行，Shizuku依赖就靠它下载
     }
 }
 
